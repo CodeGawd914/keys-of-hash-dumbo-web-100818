fd2ae74e4196fd65af09fed9392a82c7dd2fb2b6
Travis Desir <travisgdesir31@gmail.com>
2018-09-24 18:57:24 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,7 +1,7 @@
 class Hash
   def keys_of(*arguments)
     array = []
-      arguments.each do |animal,country|
+      hash.each do |animal,country|
         if arguments.include?(country)
           array.push(animal)
         end
