825cf4af5312da43a9a397a884e8e9c28e37cf0c
Travis Desir <travisgdesir31@gmail.com>
2018-09-24 19:16:52 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,12 +1,10 @@
 require 'pry'
 class Hash
   def keys_of(*arguments)
-    array = []
       map do |x,y|
         if arguments.include?(y)
           array << x
         end
       end
-      array
     end
 end
