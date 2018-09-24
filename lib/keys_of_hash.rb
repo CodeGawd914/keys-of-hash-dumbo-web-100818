da43d44932c8e4a6113546b88274057e51815318
Travis Desir <travisgdesir31@gmail.com>
2018-09-24 19:21:53 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,15 +1,15 @@
 require 'pry'
-# class Hash
-#   def keys_of(*arguments)
-#       map do |x,y|
-#         if arguments.include?(y)
-#         return x
-#         else
-#         return nil
-#         end
-#       end.flatten
-#     end
-# end
+class Hash
+  def keys_of(*arguments)
+      map do |x,y|
+      if arguments.include?(y)
+        return x
+        else
+        return nil
+        end
+      end.flatten
+    end
+end
 
 class Hash
   def keys_of(*args)
